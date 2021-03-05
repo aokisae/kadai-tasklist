aa640c1dbca6b0958cf0237dd9cc9f6f4b3823b2
aoki.sae <muzyunkisa@yahoo.co.jp>
2021-03-05 04:22:35 +0000

    CRUDteisei

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show,:destroy]
   def index
       @task = current_user.tasks.order(id: :desc).page(params[:page])
   end
